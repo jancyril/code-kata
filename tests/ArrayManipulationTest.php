5053c95e67b35db7f5025eeb15651ac1fe0d8e5d
Jan Cyril Segubience <jancyrilsegubience@gmail.com>
2016-07-30 23:23:33 +0800

    Consolidate two solutions in single test

--- tests/ArrayManipulationTest.php
+++ tests/ArrayManipulationTest.php
@@ -14,17 +14,6 @@ class ArrayManipulationTest extends PHPUnit_Framework_TestCase
         $expected = [18, 63, 80, 25, 32, 43, 80, 93, 80, 25, 93, -1, 28, -1, -1];
 
         $this->assertEquals($expected, $manipulator->firstSolution($data));
-    }
-
-    /** @test  **/
-    
-    public function this_will_do_the_same_as_with_the_first_test_but_by_using_a_collection()
-    {
-        $manipulator = new ArrayManipulation;
-
-        $data = [8, 58, 71, 18, 31, 32, 63, 92, 43, 3, 91, 93, 25, 80, 28];
-        $expected = [18, 63, 80, 25, 32, 43, 80, 93, 80, 25, 93, -1, 28, -1, -1];
-
         $this->assertEquals($expected, $manipulator->secondSolution($data));
     }
 }
