73f2e83280023179863422ef9d78d655cde449a4
Jan Cyril Segubience <jancyrilsegubience@gmail.com>
2016-07-30 22:58:09 +0800

    Problem solved for array manipulation
    Provided two solutions

--- src/ArrayManipulation.php
+++ src/ArrayManipulation.php
@@ -1,11 +1,72 @@
 <?php
 
-namespace JC;
+namespace Janitor;
+
+use Illuminate\Support\Collection;
 
 class ArrayManipulation
 {
-    public function manipulate($data = [])
+
+    /**
+     * Vanilla PHP solution
+     * @param  array  $data
+     * @return array
+     */
+    public function firstSolution($data = [])
+    {   
+        $manipulated = [];
+
+        foreach ($data as $key => $value) {
+            $replacement = -1;
+
+            for ($x = 0; $x < count($data); $x++) {
+                $temp = 0;
+                if ($key >= $x) {
+                    continue;
+                }
+
+                if ($data[$x] < $value) {
+                    continue;
+                }
+
+                $temp = $data[$x];
+
+                for ($y = 0; $y < count($data); $y++) {
+                    if ($temp > $data[$y] && $data[$y] > $value && $y > $key) {
+                        $temp = $data[$y];
+                    }
+                }
+                
+                $replacement = $temp;
+            }
+
+            array_push($manipulated, $replacement);
+        }
+
+        return $manipulated;
+    }
+
+    /**
+     * Shorter solution using illuminate collection
+     * @param  array  $data
+     * @return array
+     */
+    public function secondSolution($data = [])
     {
+        $sorted = collect($data)->sort();
         
+        return collect($data)->map(function ($value, $index) use ($sorted) {
+            return $sorted->filter(function ($item, $key) use ($value, $index) {
+                if (($item > $value) && ($key > $index)) {
+                    return $item;
+                }
+            })->sort()->first();
+        })->transform(function ($value) {
+            if ($value == null) {
+                return -1;
+            }
+
+            return $value;
+        })->all();
     }
 }
