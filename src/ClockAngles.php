b2ef593389824451d1e5361f0710df9b55bca96b
Jan Cyril Segubience <jancyrilsegubience@gmail.com>
2016-08-01 13:06:45 +0800

    Clock Angles

---
+++ src/ClockAngles.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Janitor;
+
+/**
+ * Write a program that, given a time as hours and minutes (using a 12-hour clock),
+ * calculates the angle between the two hands. For instance, at 2:00 the angle is 60°.
+ * 
+ * See https://programmingpraxis.com/2016/07/01/clock-angles/
+ */
+class ClockAngles
+{
+    public function calculate($time)
+    {
+        $time = explode(':', trim($time));
+        $short = $this->computeShortHand($time[0], $time[1]);
+        $long = $this->computeLongHand($time[1]);
+
+        return abs($short - $long);
+    }
+
+    private function computeShortHand($hour, $minute)
+    {
+        return (60 * $hour + $minute) / 2;
+    }
+
+    private function computeLongHand($minute)
+    {
+        return 6 * $minute;
+    }
+}
---
+++ tests/ClockAnglesTest.php
@@ -0,0 +1,24 @@
+<?php
+
+use Janitor\ClockAngles;
+
+class ClockAnglesTest extends PHPUnit_Framework_TestCase
+{
+    /** @test */
+
+    public function it_will_return_60()
+    {
+        $angle = new ClockAngles;
+
+        $this->assertEquals(60, $angle->calculate('2:00'));
+    }
+
+    /** @test  **/
+    
+    public function it_will_return_212_point_5()
+    {
+        $angle = new ClockAngles;
+
+        $this->assertEquals(212.5, $angle->calculate('3:55'));
+    }
+}
